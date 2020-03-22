4b77e847b2fe663078d4fe748a3adfbd9848ceb7
Simon Lydell <simon.lydell@gmail.com>
2020-03-22 20:14:57 +0100

    Add initial TypeScript definition

---
+++ index.d.ts
@@ -0,0 +1,18 @@
+declare type Token =
+  | { type: "StringLiteral"; value: string; closed: boolean }
+  | { type: "NoSubstitutionLiteral"; value: string; closed: boolean }
+  | { type: "TemplateHead"; value: string }
+  | { type: "TemplateMiddle"; value: string }
+  | { type: "TemplateTail"; value: string; closed: boolean }
+  | { type: "MultiLineComment"; value: string; closed: boolean }
+  | { type: "SingleLineComment"; value: string }
+  | { type: "RegularExpressionLiteral"; value: string }
+  | { type: "NumericLiteral"; value: string }
+  | { type: "Punctuator"; value: string }
+  | { type: "WhiteSpace"; value: string }
+  | { type: "LineTerminatorSequence"; value: string }
+  | { type: "Invalid"; value: string };
+
+declare function tokenize(input: string): Iterable<Token>;
+
+export default tokenize;
