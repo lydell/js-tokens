436d9778a0d037c43e278c5ffc23179979ae87fe
Simon Lydell <simon.lydell@gmail.com>
2020-04-04 11:16:51 +0200

    Merge fixtures.test.js and test262.test.js

--- test/fixtures.test.js
+++ test/fixtures.test.js
@@ -1,20 +1,173 @@
 "use strict";
 
 const fs = require("fs");
-const { default: jsTokens } = require("../package/index");
+const path = require("path");
+const babelParser = require("@babel/parser");
+const { default: jsTokensLib } = require("../package/index");
 
-describe("fixtures", () => {
-  function testFile(file) {
-    const contents = fs.readFileSync(`test/fixtures/${file}.js`).toString();
-    const expected = require(`./fixtures/${file}.json`);
-    const actual = Array.from(jsTokens(contents), (t) => t.value);
-    test(`${file}.js`, () => {
-      expect(actual).toEqual(expected);
-      expect(actual.join("")).toBe(contents);
+const babelTypeMap = {
+  bigint: "NumericLiteral",
+  CommentBlock: "MultiLineComment",
+  CommentLine: "SingleLineComment",
+  name: "IdentifierName",
+  num: "NumericLiteral",
+  regexp: "RegularExpressionLiteral",
+  string: "StringLiteral",
+};
+
+function babel(code, sourceType) {
+  const { tokens } = babelParser.parse(code, {
+    tokens: true,
+    sourceType,
+  });
+
+  const result = [];
+  for (let index = 0; index < tokens.length; index++) {
+    const token = tokens[index];
+
+    if (token.type.label === "`") {
+      const next2 = tokens[index + 2];
+      result.push({
+        type:
+          next2.type.label === "`" ? "NoSubstitutionTemplate" : "TemplateHead",
+        value: code.slice(token.start, next2.end),
+      });
+      index += 2;
+      continue;
+    }
+
+    if (token.type.label === "}") {
+      const next1 = tokens[index + 1];
+      const next2 = tokens[index + 2];
+      if (next1 && next1.type.label === "template") {
+        result.push({
+          type: next2.type.label === "`" ? "TemplateTail" : "TemplateMiddle",
+          value: code.slice(token.start, next2.end),
+        });
+        index += 2;
+        continue;
+      }
+    }
+
+    if (token.type.label === "eof") {
+      continue;
+    }
+
+    const value = code.slice(token.start, token.end);
+
+    result.push({
+      type:
+        token.type === "CommentLine" &&
+        (value.startsWith("<!--") || value.startsWith("-->"))
+          ? "HTMLLikeComment"
+          : babelTypeMap[token.type.label || token.type] ||
+            (token.type.keyword !== undefined
+              ? "IdentifierName"
+              : "Punctuator"),
+      value,
     });
   }
 
-  testFile("base64");
-  testFile("errors");
-  testFile("slashes");
+  return result;
+}
+
+function readJsonIfExists(file) {
+  return fs.existsSync(file)
+    ? JSON.parse(fs.readFileSync(file, "utf8"))
+    : undefined;
+}
+
+function runFile(file, { compareWithBabel = true } = {}) {
+  const code = fs.readFileSync(file, "utf8");
+  const json = readJsonIfExists(file.replace(/\.[jt]s$/, ".json"));
+
+  const sourceType = /[.-]module\.[jt]s$/.test(file) ? "module" : "script";
+  const isTypeScript = file.endsWith(".ts");
+
+  const features = [
+    isTypeScript ? "TS" : "JS",
+    sourceType,
+    compareWithBabel ? "Babel" : undefined,
+    json ? "JSON" : undefined,
+  ]
+    .filter(Boolean)
+    .join("/");
+
+  test(`${features}: ${file}`, () => {
+    const babelTokens = compareWithBabel
+      ? babel(code, sourceType, isTypeScript)
+      : [];
+
+    const jsTokens = Array.from(jsTokensLib(code), ({ type, value }) => ({
+      type,
+      value,
+    }));
+
+    const jsTokensValues = jsTokens.map((token) => token.value);
+
+    const jsTokensWithoutBlanks = jsTokens.filter(
+      (token) => !/^\s*$/.test(token.value)
+    );
+
+    if (babelTokens.some((token) => token.type === "HTMLLikeComment")) {
+      return;
+    }
+
+    if (compareWithBabel) {
+      expect(jsTokensWithoutBlanks).toEqual(babelTokens);
+    }
+
+    if (json) {
+      expect(jsTokensValues).toEqual(json);
+    }
+
+    expect(jsTokensValues.join("")).toBe(code);
+  });
+}
+
+function getFiles(dir) {
+  return fs
+    .readdirSync(dir)
+    .filter((file) => /\.[jt]s$/.test(file))
+    .map((file) => path.join(dir, file));
+}
+
+const valid = [
+  "./test/fixtures/valid",
+  "./node_modules/everything.js",
+  "./node_modules/test262-parser-tests/pass",
+  "./node_modules/test262-parser-tests/pass-explicit",
+];
+
+const invalid = [
+  "./test/fixtures/invalid",
+  "./node_modules/test262-parser-tests/fail",
+  "./node_modules/test262-parser-tests/early",
+];
+
+const ignored = new Set([
+  // Known cases where js-tokens mis-identifies regex as division.
+  "node_modules/test262-parser-tests/pass/9d3d960e32528788.js",
+  "node_modules/test262-parser-tests/pass/d53aef16fe683218.js",
+  "node_modules/test262-parser-tests/pass-explicit/d53aef16fe683218.js",
+]);
+
+describe("valid", () => {
+  for (const dir of valid) {
+    for (const file of getFiles(dir)) {
+      if (!ignored.has(file)) {
+        runFile(file);
+      }
+    }
+  }
+});
+
+describe("invalid", () => {
+  for (const dir of invalid) {
+    for (const file of getFiles(dir)) {
+      if (!ignored.has(file)) {
+        runFile(file, { compareWithBabel: false });
+      }
+    }
+  }
 });
