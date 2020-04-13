986dd51c67f069b6bafcbebd96e004431ceceb67
Simon Lydell <simon.lydell@gmail.com>
2020-04-13 13:33:15 +0200

    Improve benchmark

--- benchmark.js
+++ benchmark.js
@@ -6,39 +6,51 @@ const fs = require("fs");
 const jsTokens = require("./build/index");
 
 const usage = `
-Usage:
+Usage: node benchmark.js [TOKENIZER] [NUM_FILES]
+
+TOKENIZER:
+- js-tokens (default)
+- babel
+
+NUM_FILES:
+Positive integer.
 
 # Run almost everything in node_modules through js-tokens:
 node benchmark.js
 
-# Run the first million lines in node_modules through js-tokens:
-node benchmark.js 1e6
-
-# Run almost everything in node_modules through @babel/parser:
-node benchmark.js babel
+# Run the 1000 shortest, non-empty JS files in node_modules through @babel/parser:
+node benchmark.js babel 1000
 `.trim();
 
-function parseOption(string) {
-  if (string === "babel") {
-    return "babel";
+function parseTokenizer(string) {
+  switch (string) {
+    case "babel":
+    case "js-tokens":
+      return string;
+    default:
+      throw new Error(`Expected "js-tokens" or "babel", but got: ${string}`);
   }
+}
 
+function parseNumFiles(string) {
   const number = Number(string);
   if (Number.isFinite(number) && Number.isInteger(number) && number > 0) {
     return number;
   }
 
-  throw new Error(`Expected "babel" or a positive integer, but got: ${string}`);
+  throw new Error(`Expected a positive integer, but got: ${string}`);
 }
 
 function parseArguments(argv) {
   switch (argv.length) {
     case 0:
-      return undefined;
+      return ["js-tokens", Infinity];
     case 1:
-      return parseOption(argv[0]);
+      return [parseTokenizer(argv[0]), Infinity];
+    case 2:
+      return [parseTokenizer(argv[0]), parseNumFiles(argv[1])];
     default:
-      throw new Error(`Expected 0 or 1 argument, but got: ${argv.length}`);
+      throw new Error(`Expected 0 to 2 arguments, but got: ${argv.length}`);
   }
 }
 
@@ -47,37 +59,84 @@ function read() {
     .sync("node_modules/**/*.js", {
       ignore: ["node_modules/test262-parser-tests"],
     })
-    .map((file) => fs.readFileSync(file, "utf8"))
-    .join("\n;\n")
-    .replace(/\n#.*/g, "\n");
+    .map((file) => [
+      file,
+      fs.readFileSync(file, "utf8").replace(/^#!.*\r?\n/, ""),
+    ])
+    .filter(
+      ([, content]) =>
+        /\S/.test(content) &&
+        (content.trim().includes("\n") || content.length > 100)
+    )
+    .sort(
+      ([fileA, contentA], [fileB, contentB]) =>
+        contentA.length - contentB.length || fileA.localeCompare(fileB)
+    )
+    .map(([file, content]) => `/*${file.replace(/\*\//g, "* /")}*/ ${content}`);
+}
+
+function mean(array) {
+  return array.reduce((a, b) => a + b, 0) / array.length;
+}
+
+function median(array) {
+  const sorted = array.slice().sort((a, b) => a - b);
+  const half = sorted.length / 2;
+  return half === 0
+    ? 0
+    : Number.isInteger(half)
+    ? (sorted[half - 1] + sorted[half]) / 2
+    : sorted[Math.floor(half)];
+}
+
+function common(array) {
+  const map = new Map();
+  for (const i of array) {
+    map.set(i, (map.get(i) || 0) + 1);
+  }
+  return Array.from(map.entries())
+    .sort(([, a], [, b]) => b - a)
+    .slice(0, 4);
+}
+
+function nonEmptyLines(string) {
+  return string.split("\n").filter((line) => line.length > 0);
 }
 
 function run(argv) {
-  const option = parseArguments(argv);
-  const allCode = read();
-  const lines = allCode.split("\n");
-  const code =
-    typeof option === "number" ? lines.slice(0, option).join("\n") : allCode;
-  const name = option === "babel" ? "babel" : "js-tokens";
+  const [tokenizer, numFiles] = parseArguments(argv);
+  const allFiles = read();
+  const files = allFiles.slice(0, numFiles);
+  const allCode = allFiles.join("\n;");
+  const code = files.join("\n;");
+  const fileLengths = files.map((file) => file.length);
+  const lineLengths = nonEmptyLines(code).map((line) => line.length);
 
+  console.log("Total files:", allFiles.length);
   console.log("Total chars:", allCode.length);
-  console.log("Total lines:", lines.length);
+  console.log("Total lines:", nonEmptyLines(allCode).length);
   if (code !== allCode) {
+    console.log("Used files: ", files.length);
     console.log("Used chars: ", code.length);
-    console.log("Used lines: ", option);
+    console.log("Used lines: ", nonEmptyLines(code).length);
   }
 
-  console.log(
-    "Average line length:",
-    lines.map((line) => line.length).reduce((a, b) => a + b, 0) / lines.length
-  );
+  console.log("File length mean:  ", mean(fileLengths));
+  console.log("File length median:", median(fileLengths));
+  console.log("File length common:", common(fileLengths));
+  console.log("Line length mean:  ", mean(lineLengths));
+  console.log("Line length median:", median(lineLengths));
+  console.log("Line length common:", common(lineLengths));
 
-  console.time(name);
-  if (option === "babel") {
+  fs.writeFileSync("benchmark.input.js", code);
+
+  console.time(tokenizer);
+  if (tokenizer === "babel") {
     try {
       const { tokens } = babelParser.parse(code, {
         tokens: true,
         errorRecovery: true,
+        plugins: ["jsx", "flow"],
       });
       console.log("Number of tokens:", tokens.length);
     } catch (error) {
@@ -86,18 +145,19 @@ function run(argv) {
   } else {
     // `Array.from` causes an out-of-memory crash in Node.js 10.
     const tokens = [];
-    for (const token of jsTokens(code)) {
+    for (const token of jsTokens(code, { jsx: true })) {
       tokens.push(token);
     }
     console.log("Number of tokens:", tokens.length);
   }
-  console.timeEnd(name);
+  console.timeEnd(tokenizer);
 }
 
 try {
   run(process.argv.slice(2));
 } catch (error) {
   console.error(error.message);
+  console.log();
   console.log(usage);
   process.exit(1);
 }
